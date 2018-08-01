1345d8391c16880d6c0b853a3cf6e44491e50435
asgordon <gordon@ict.usc.edu>
2018-08-01 12:23:47 -0700

    Fixed example 3

--- docs/lemon-lime.lisp
+++ docs/lemon-lime.lisp
@@ -1,9 +1,30 @@
 ;; Example 3: Lemon or Lime ? 
 
+;; Observables: Four different fruit
+
+(yellow FRUIT1)
+(not_round FRUIT1)
+
+(yellow FRUIT2)
+(round FRUIT2)
+
+(not_yellow FRUIT3)
+(not_round FRUIT3)
+
+(not_yellow FRUIT4)
+(round FRUIT4)
+
 ;; Prior probabilities of class labels
 
 (if (etc0_lime 0.6 x) (lime x)) ;; 600 / 1000
-(if (etc0_lemmon 0.4 x) (lemon x)) ;; 400 / 1000
+(if (etc0_lemon 0.4 x) (lemon x)) ;; 400 / 1000
+
+;; Prior probabilities of features
+
+(if (etc0_yellow 0.9 x) (yellow x))
+(if (etc0_not_yellow 0.1 x) (not_yellow x))
+(if (etc0_round 0.33 x) (round x))
+(if (etc0_not_round 0.67 x) (not_round x))
 
 ;; conditional probabilities of the features given the class
 
@@ -31,45 +52,4 @@
 (if (and (lemon x) (etc2_not_round 0.925 x)) ;; 370 / 400
     (not_round x))
 
-;; Observables: Four different fruit
-
-(yellow FRUIT1)
-(not_round FRUIT1)
-
-(yellow FRUIT2)
-(round FRUIT2)
-
-(not_yellow FRUIT3)
-(not_round FRUIT3)
-
-(not_yellow FRUIT4)
-(round FRUIT4)
-
-
-;; Bonus example: What about a tree?
-;; Previously specified prior probabilities must be commented out, above
-
-;; Prior probabilities of new class labels
-; (if (etc0_lime_tree 0.1 x) (lime_tree x))
-; (if (etc0_lemon_tree 0.1 x) (lemon_tree x))
-
-;; Most lime trees produce limes (90%)
-; (if (and (lime_tree y)
-; 	 (etc1_lime 0.9 x y))
-;      (lime x))
-
-;; Most lemon trees produce lemons (90%)
-; (if (and (lemon_tree y)
-; 	 (etc1_lemon 0.9 x y))
-;     (lemon x))
-
-
-;; Extra bonus example: From classification to interpretation
-
-;; Add the prior probabilities of features (and uncomment out the priors above)
-
-; (if (etc0_yellow 0.9 x) (yellow x))
-; (if (etc0_not_yellow 0.1 x) (not_yellow x))
-; (if (etc0_round 0.33 x) (round x))
-; (if (etc0_not_round 0.67 x) (not_round x))
 
